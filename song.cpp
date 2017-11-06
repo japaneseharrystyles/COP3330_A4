00852d6881563e1e4bab0c70223e7acf16754b60
japaneseharrystyles <tristenhicks@icloud.com>
2017-11-06 13:48:33 -0500

    reverted

--- song.cpp
+++ song.cpp
@@ -1,4 +1,6 @@
 #include "song.h"
+#include <cstring>
+#include <iostream>
 
 Song::Song() {
   title[0] = '\0';
@@ -8,14 +10,23 @@ Song::Song() {
 }
 
 ostream& operator << (ostream& os, const Song& s) {
-  std::cout << s.title;
-  std::cout << s.artist;
-  std::cout << s.category;
-  std::cout << s.size;
+
+  os << "Title: " << s.title << std::endl;
+  os << "Artist: " << s.artist << std::endl;
+  os << "Category: " << s.category << std::endl;
+  os << "Size: " << s.size << std::endl;
+  return os;
 // this isn't properly formatted output yet
 // it would be hard to make adjustments since any song
 // object would currently contain empty title and artist strings
 //
 //  ******* FORMAT LATER *******
+}
+
+void Song::Set(const char* t, const char* a, Style st, int sz) {
+  strcpy (title, t);
+  strcpy (artist, a);
+  category = st;
+  size = sz;
 }
 
